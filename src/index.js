3f677f93c58701f865a6638eeda02be4606b1ec5
amyyan210 <amyyan210@gmail.com>
2017-09-13 12:08:41 -0500

    Include modules using require

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-const $ = require("jquery");
+const $ = require("jquery"); // loading jquery.min.js, third party module
+
+const data = require("./data"); // use ./ before file name for custom module
 
 const hello = () => console.log("Hello world from webpack!");
     hello();
+
+console.log(data.whichSideOfTheForce);
 
 // npm run build in terminal before being able to see for the first time
 $("body").html("<h1>Hello from jQuery</h1>").css("background-color", "gray");
